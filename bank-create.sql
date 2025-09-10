68d58f1a45e6055f908522be7737bb555c56e424
Kees van Loenen <kees.vanloenen@infosupport.com>
2025-09-10 16:40:06 +0200

    t/m eind dag 1

--- bank-create.sql
+++ bank-create.sql
@@ -1,15 +1,13 @@
 ﻿USE master;
 GO
 
+-- Sluit eerst alle openstaande connecties
 ALTER DATABASE Bank
 SET SINGLE_USER
 WITH ROLLBACK IMMEDIATE;
 GO
 
 DROP DATABASE IF EXISTS Bank;
-GO
-
-CREATE SCHEMA Hr;
 GO
 
 CREATE DATABASE Bank;
@@ -20,28 +18,52 @@ GO
 
 DROP TABLE IF EXISTS Accounts;
 DROP TABLE IF EXISTS Transactions;
+DROP TABLE IF EXISTS Hr.Employees;
+GO
+
+DROP SCHEMA IF EXISTS Hr;
 GO
 
 DROP SEQUENCE IF EXISTS TransactionSeq;
+GO
+
+CREATE SCHEMA Hr;
+GO
 
 CREATE SEQUENCE TransactionSeq
 	START WITH 1
 	INCREMENT BY 1;
 GO
 
+
+-- Herbruikbaar data type
 CREATE TYPE MoneyAmount FROM decimal(18,2) NOT NULL;
 GO
 
+-- Twee CHECK Constraints:
+-- 1. (kolom niveau) Iedereen in de tabel moet volwassen zijn (Age >= 18)
+-- 2. (tabel niveau) Iedere Nederlander (CountryCode = 'NL') moet tenminste 21 zijn
 CREATE TABLE Accounts
 (
-	Id		int IDENTITY 
-			CONSTRAINT PK_Accounts_Id 
-			PRIMARY KEY,
-	Name	nvarchar(100) NOT NULL
+	Id				int IDENTITY 
+					CONSTRAINT PK_Accounts_Id 
+					PRIMARY KEY,
+	Name			nvarchar(100) NOT NULL,
+	Age				smallint NOT NULL
+					CONSTRAINT CHK_Accounts_IsAdult
+					CHECK(Age >= 18),
+	CountryCode		char(2),
+					-- ALS CountryCode is 'NL', dan Age >= 21
+					CONSTRAINT CHK_Accounts_CountryCodeAgeLogic
+					CHECK (NOT (CountryCode = 'NL' AND Age < 21)) 
+					-- CHECK (IIF(CountryCode = 'NL' AND Age < 21, 0, 1) = 1)
+					-- CHECK (CASE WHEN (CountryCode = 'NL' AND Age < 21) THEN 0 ELSE 1 END = 1)
+					-- CHECK (CountryCode <> 'NL' OR age >= 21)
 );
 GO
 
--- nvarchar(100) 👈+   
+-- Uitleg string datatypes:
+-- nvarchar(100) 👈
 -- varchar(100)
 -- nchar(2)
 -- char(2) 👈
@@ -49,11 +71,12 @@ GO
 
 INSERT INTO Accounts
 VALUES
-('Ab'),
-('Bo'),
-('Cas');
+('Ab', 26, 'NL'),
+('Bo', 24, 'DE'),
+('Cas', 28, 'AU');
 GO
 
+-- Onderstaand is meestal een slecht idee
 --SET IDENTITY_INSERT Accounts ON;
 
 --INSERT INTO Accounts
@@ -65,6 +88,8 @@ GO
 
 GO
 
+-- UNIQUE Constraint: elk account mag maar 1 transactie per dag doen
+--                    hierbij een computed column geïntroduceerd
 CREATE TABLE Transactions
 (
 	Id					bigint DEFAULT (NEXT VALUE FOR TransactionSeq)
@@ -73,31 +98,79 @@ CREATE TABLE Transactions
 	AccountId			int NOT NULL,
 	Amount				MoneyAmount,
 	AmountWithFee		AS (CAST(Amount * 1.02 AS decimal(18,2))) PERSISTED,
-	TransactionDate		datetime2(0) NOT NULL DEFAULT SYSDATETIME(),
-						CONSTRAINT FK_Transactions_AccountId
+	TransactionDate		datetime2(0) NOT NULL 
+						CONSTRAINT DF_Transactions_TransActionDateToday
+						DEFAULT SYSDATETIME(),
+	TransactionDay		AS (CAST(TransactionDate AS date)) PERSISTED,	-- hulpkolom voor UNIQUE constraint
+						CONSTRAINT FK_Transactions_AccountId			-- FOREIGN KEY constraint
 						FOREIGN KEY (AccountId) REFERENCES Accounts(Id)
 						ON DELETE CASCADE
-						ON UPDATE CASCADE
+						ON UPDATE CASCADE,
+						CONSTRAINT UQ_Transactions_AccountId_TransactionDate	-- UNIQUE constraint
+						UNIQUE (AccountId, TransactionDay)
 );
 GO
 
-DELETE FROM Accounts WHERE Id = 1
+-- Hoe om te gaan met kolommen met DEFAULT value:
+-- manier 1:
+INSERT INTO Transactions
+(AccountId, Amount)						-- laat nadrukkelijk de kolomnaam en value weg
+VALUES
+(1, 100);
+GO
+
+-- manier 2:
+INSERT INTO Transactions
+(AccountId, Amount, TransactionDate)	-- geef kolomnaam en als waarde DEFAULT
+VALUES
+(2, 100, DEFAULT);
+GO
+
 
 INSERT INTO Transactions
 (AccountId, Amount, TransactionDate)
 VALUES
-(1, 100.00, DATEADD(DAY, -3, SYSDATETIME())),
-(2, 150.50, DATEADD(DAY, -2, SYSDATETIME())),
-(3, 200.75, DATEADD(DAY, -1, SYSDATETIME())),
-(1, 120.00, SYSDATETIME()),
-(2, 180.25, DATEADD(DAY, 1, SYSDATETIME()));  -- één dag in de toekomst als voorbeeld
+(1, 100.00, '2025-01-01T22:23:56'),
+(2, 150.50, '2025-05-01T22:23:56'),
+(3, 200.75, '2025-02-01T22:23:56'),
+(1, 120.00, '2025-03-01T22:23:56'),
+(2, 180.25, '2025-04-01T22:23:56');
 
 GO
 
+-- Gebruik van eerder gemaakte SCHEMA
 CREATE TABLE Hr.Employees
 (
+    Id				int IDENTITY 
+					CONSTRAINT PK_Employees_Id 
+					PRIMARY KEY,
+	Voornaam		nvarchar(75),
+	GeboorteDatum	date
+					CONSTRAINT CHK_Employees_IsRealistischeGeboortedatum
+					CHECK
+					(
+						Geboortedatum BETWEEN 
+						DATEADD(year, -80, SYSDATETIME()) 
+						AND DATEADD(year, -17, SYSDATETIME())
+					)
 );
+
 GO
 
+SELECT 
+	a.Name
+	, a.Age
+	, a.CountryCode
+	, t.Amount
+	, t.AmountWithFee
+	, t.TransactionDate
+	, t.TransactionDay
+FROM Accounts AS a
+INNER JOIN Transactions AS t
+ON a.Id = t.AccountId;
 
+--------------------------------------------------------------------------------------------------------
+
+-- Bij het aanmaken van een nieuwe transactie moet er een check plaatsvinden:
+-- Als het om een Nederlands account gaat, moet het minimale bedrag 50.00 zijn
 
---
+++ dag1-functions.sql
@@ -0,0 +1,69 @@
+﻿-- Dag 1, middag functions
+
+-- Een UDF is een User Defined Function
+-- Het is een SCALAR function (die indien gebruikt binnen een tabel)
+-- voor elke rij wordt uitgevoerd
+
+GO
+CREATE OR ALTER FUNCTION dbo.IsEven
+(@number AS int)
+RETURNS bit
+AS
+BEGIN
+	RETURN IIF(@number % 2 = 0, 1, 0);
+END;
+GO
+
+SELECT 
+	dbo.IsEven(10) AS Verwacht1
+	, dbo.IsEven(9) AS Verwacht0;
+
+
+-- IsStrongPassword() function:
+-- min. 12 tekens
+-- min. 1 cijfer
+-- min. 1 hoofdletter (🤯)
+
+GO
+
+CREATE OR ALTER FUNCTION dbo.ContainsCapitalLetter 
+(@input nvarchar(MAX))
+RETURNS bit
+AS
+BEGIN
+    DECLARE @index int = 1;
+
+    WHILE @index <= LEN(@input)
+    BEGIN
+        -- UNICODE() returns ASCII Code: 65 = 'A', 90 = 'Z'
+        IF UNICODE(SUBSTRING(@input, @index, 1)) BETWEEN 65 AND 90
+            RETURN 1;
+
+        SET @index += 1;
+    END
+
+    RETURN 0;
+END
+GO
+
+CREATE OR ALTER FUNCTION dbo.IsStrongPassword
+(@password AS nvarchar(100))
+RETURNS bit
+AS
+BEGIN
+	RETURN (
+		CASE
+			WHEN LEN(@password) < 12 THEN 0
+			WHEN @password NOT LIKE '%[0-9]%' THEN 0
+			WHEN dbo.ContainsCapitalLetter(@password) = 0 THEN 0
+			ELSE 1
+		END
+	)
+END;
+
+GO
+
+SELECT 
+	dbo.IsStrongPassword('Welkom123')     AS R1_0,
+    dbo.IsStrongPassword('welkom123456')  AS R2_0,
+	dbo.IsStrongPassword('welKom123456')  AS R3_1
